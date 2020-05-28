2691e4b3ec1058b5362ef23e0655145d1c91e79e
mazsak <mazsak97@gmail.com>
2020-05-28 13:22:22 +0200

    init projects

---
+++ src/test/java/com/project/movie_catalog/MovieCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.movie_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
