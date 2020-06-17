e3b47860376d5d8d34b5de0043c332a2376131be
adam <adampolejczuk@gmail.com>
2020-06-17 16:34:40 +0200

    Add: actor services

--- src/main/java/com/project/movie_catalog/controller/ActorController.java
+++ src/main/java/com/project/movie_catalog/controller/ActorController.java
@@ -1,6 +1,6 @@
 package com.project.movie_catalog.controller;
 
-import com.project.movie_catalog.form.ActorForm;
+import com.project.movie_catalog.form.*;
 import com.project.movie_catalog.service.ActorService;
 import com.project.movie_catalog.service.ActorServiceImpl;
 import org.springframework.web.bind.annotation.*;
@@ -19,10 +19,38 @@ public class ActorController {
     }
 
     @GetMapping("")
-    public List<ActorForm> getAllActors(@RequestParam(defaultValue = "0") Integer page,
-                                       @RequestParam(defaultValue = "10") Integer size,
-                                       @RequestParam(defaultValue = "title") String sortBy) {
+    public ActorPageSimpleForm getAllActors(@RequestParam(defaultValue = "0") Integer page,
+                                            @RequestParam(defaultValue = "10") Integer size,
+                                            @RequestParam(defaultValue = "name") String sortBy) {
 
         return actorService.findAll(page, size, sortBy);
+    }
+
+    @GetMapping("/{id}")
+    public ActorForm getActor(@PathVariable String id) {
+        return actorService.findById(id);
+    }
+
+    @RequestMapping(value = "/filter", params = "name", method = RequestMethod.GET)
+    public ActorPageSimpleForm findAllByName(@RequestParam(defaultValue = "0") Integer page,
+                                         @RequestParam(defaultValue = "10") Integer size,
+                                         @RequestParam(defaultValue = "name") String sortBy,
+                                         @RequestParam String name) {
+        return actorService.findAllByName(page, size, sortBy, name);
+    }
+
+    @PostMapping
+    public ActorForm create(@RequestBody ActorForm actorForm) {
+        return actorService.saveAndReturn(actorForm);
+    }
+
+    @PutMapping("/{id}")
+    public ActorForm update(@RequestBody ActorForm actorForm) {
+        return actorService.saveAndReturn(actorForm);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable String id) {
+        actorService.delete(id);
     }
 }
--- src/main/java/com/project/movie_catalog/service/ActorServiceImpl.java
+++ src/main/java/com/project/movie_catalog/service/ActorServiceImpl.java
@@ -1,9 +1,13 @@
 package com.project.movie_catalog.service;
 
 import com.project.movie_catalog.form.ActorForm;
+import com.project.movie_catalog.form.ActorPageSimpleForm;
+import com.project.movie_catalog.form.FilmPageSimpleForm;
 import com.project.movie_catalog.mapper.ActorMapper;
 import com.project.movie_catalog.model.Actor;
+import com.project.movie_catalog.model.Film;
 import com.project.movie_catalog.repo.ActorRepo;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -21,8 +25,36 @@ public class ActorServiceImpl extends BasicServiceImpl<Actor, ActorForm, ActorRe
     }
 
     @Override
-    public List<ActorForm> findAll(Integer page, Integer size, String sortBy) {
+    public ActorPageSimpleForm findAll(Integer page, Integer size, String sortBy) {
         Pageable paging = PageRequest.of(page, size, Sort.by(sortBy));
-        return mapper.mapToDTOList(repo.findAll(paging).getContent());
+        Page<Actor> actors = repo.findAll(paging);
+        int totalPages = actors.getTotalPages();
+        if (totalPages <= page && totalPages != 0) {
+            page = totalPages - 1;
+            paging = PageRequest.of(page, size, Sort.by(sortBy));
+            actors = repo.findAll(paging);
+        }
+        return ActorPageSimpleForm.builder()
+                .totalPages(totalPages)
+                .currentPage(page)
+                .actors(mapper.mapToDTOList(actors.getContent()))
+                .build();
+    }
+
+    @Override
+    public ActorPageSimpleForm findAllByName(Integer page, Integer size, String sortBy, String title) {
+        Pageable paging = PageRequest.of(page, size, Sort.by(sortBy));
+        Page<Actor> actors = repo.findAllByNameContainingIgnoreCase(paging, title);
+        int totalPages = actors.getTotalPages();
+        if (totalPages <= page && totalPages != 0) {
+            page = totalPages - 1;
+            paging = PageRequest.of(page, size, Sort.by(sortBy));
+            actors = repo.findAllByNameContainingIgnoreCase(paging, title);
+        }
+        return ActorPageSimpleForm.builder()
+                .totalPages(totalPages)
+                .currentPage(page)
+                .actors(mapper.mapToDTOList(actors.getContent()))
+                .build();
     }
 }
